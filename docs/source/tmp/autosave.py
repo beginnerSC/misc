62956482d318201589628d0d19f3865c031fe742
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2020-10-07 21:25:54 +0000

    autosave

---
+++ docs/source/tmp/autosave.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Autosave
+# 
+# * Below is how subprocess can call an alias, code from [here](https://stackoverflow.com/questions/12060863/python-subprocess-call-a-bash-alias). 
+# * In terminal, turn this into a python script, say ```autosave.ipynb``
+#     ```
+#     jupyter nbconvert --to script autosave.ipynb
+#     ```
+# * Also in terminal ```python autosave.py```
+# * This can be used with the schedule library
+# 
+
+# In[17]:
+
+
+import subprocess
+
+if __name__ == '__main__':
+    subprocess.call(['/bin/bash', '-i', '-c', 'cnp'])
+
