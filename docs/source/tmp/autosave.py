64395960cae21bd7fa80b0c04d73ce11164a573d
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2020-10-07 22:04:06 +0000

    autosave

--- docs/source/tmp/autosave.py
+++ docs/source/tmp/autosave.py
@@ -12,11 +12,19 @@
 # * This can be used with the schedule library
 # 
 
-# In[17]:
+# In[ ]:
 
 
-import subprocess
+import schedule 
+import datetime, time, subprocess
+
+def autosave():
+    subprocess.call(['/bin/bash', '-i', '-c', 'cnp'])
+    print('Pushed', datetime.datetime.now())
 
 if __name__ == '__main__':
-    subprocess.call(['/bin/bash', '-i', '-c', 'cnp'])
+    schedule.every(5).minutes.do(autosave)
+    while True:
+        schedule.run_pending() 
+        time.sleep(1) 
 
