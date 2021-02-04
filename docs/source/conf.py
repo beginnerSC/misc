e9a8b96d5d5eb21c493508429fd81db238f480de
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2021-02-04 17:42:49 +0000

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,16 +14,16 @@ import os
 import sys
 from sphinx.ext.apidoc import main
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 
 ### for RTD to run apidoc automatically, code from https://github.com/readthedocs/readthedocs.org/issues/1139
 
 def run_apidoc(_):
-    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
     cur_dir = os.path.abspath(os.path.dirname(__file__))
-    module = os.path.join(cur_dir, '..', 'pycircle')
-    main(['-e', '-o', cur_dir, module, '--force'])
+    module = os.path.join(cur_dir, '../..', 'pycircle')
+    main(['-e', '-o', cur_dir, module, '--force'])        # cur_dir is the output path *.rst will be in
 
 def setup(app):
     app.connect('builder-inited', run_apidoc)
