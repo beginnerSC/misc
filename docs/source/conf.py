16aadad31dbd3ba078ce80a3c19b85a5245d2006
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2021-07-22 00:01:59 +0000

    autosave

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,7 +48,7 @@ extensions = [  'nbsphinx',
                 'sphinx.ext.autodoc', 
                 'sphinx.ext.napoleon',
                 'sphinx.ext.mathjax', 
-                'sphinx.ext.viewcode', 
+#                 'sphinx.ext.viewcode', 
              ]
 
 # Add any paths that contain templates here, relative to this directory.
