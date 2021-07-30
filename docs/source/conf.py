a8305735ba820330a7c69028317c0ad2a65bf635
beginnerSC <cc12s@my.fsu.edu>
2021-07-30 12:24:54 -0400

    sphinx_copybutton

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,6 +49,7 @@ extensions = [  'nbsphinx',
                 'sphinx.ext.napoleon',
                 'sphinx.ext.mathjax', 
                 'sphinx.ext.viewcode', 
+                'sphinx_copybutton'              
              ]
 
 # Add any paths that contain templates here, relative to this directory.
