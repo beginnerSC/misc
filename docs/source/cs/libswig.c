9ad98cca7799d56d1e460e721be3b24db61d9caa
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2021-09-11 14:52:31 +0000

    autosave

--- docs/source/cs/libswig.c
+++ docs/source/cs/libswig.c
@@ -1,7 +1,12 @@
+// #include "libswig.hpp"
+
+// void hello_world(){
+//     std::cout << "Hello World!" << std::endl;
+// }
+
 double square(double x){
     return x*x;
 }
-
 double cube(double x){
     return x*x*x;
 }
--- docs/source/cs/libswig.h
+++ docs/source/cs/libswig.h
@@ -1,2 +1,4 @@
+// #include<iostream>
+
 double square(double x);
 double cube(double x);
