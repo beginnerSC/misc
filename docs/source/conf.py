51a8a3a81d70a2c5ba1baacf5060720a29002525
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2025-02-24 22:11:22 -0500

    autosave

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -49,7 +49,8 @@ extensions = [  'nbsphinx',
                 'sphinx.ext.napoleon',
                 'sphinx.ext.mathjax', 
                 'sphinx.ext.viewcode', 
-                'sphinx_copybutton'              
+                'sphinx_copybutton', 
+                'sphinx_rtd_dark_mode'
              ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -67,11 +68,12 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-# import sphinx_rtd_theme
-# html_theme = 'sphinx_rtd_theme'
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
 
-import pydata_sphinx_theme
-html_theme = 'pydata_sphinx_theme'
+# import pydata_sphinx_theme
+# html_theme = 'pydata_sphinx_theme'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
