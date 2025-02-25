54c03a5966884dfd8dca1afcc7591c13228e8a89
beginnerSC <25188222+beginnerSC@users.noreply.github.com>
2025-02-24 21:48:20 -0500

    autosave

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,8 +67,11 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
+# import sphinx_rtd_theme
+# html_theme = 'sphinx_rtd_theme'
+
+import pydata-sphinx-theme
+html_theme = 'pydata-sphinx-theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
